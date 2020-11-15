38de1b331ca707f6cd639831fa807b5b7834e980
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2020-11-14 20:08:25 -0800

    Program now using modern syntax, also smaller size

--- main.js
+++ main.js
@@ -1,43 +1,17 @@
 function main() {
-    //Program variables.
-    const months = document.querySelector(`#months`);
-    const days = document.querySelector(`#days`);
-    const years = document.querySelector(`#years`);
-    const selection = document.querySelector(`#selection`);
+    //Primary program variables.
+    const datePicker = document.querySelector(`#datePicker`);
     const submit = document.querySelector(`#submit`);
     const timer = document.querySelector(`#timer`);
-    //Making sure a given selected date exists, given the number of days in each month. Is a support function.
-    function checkDays() {
-        const mV = Number(months.value);
-        const dV = Number(days.value);
-        if (mV === 1 && dV > 28) {
-            return false;
-        } 
-        if ((mV === 3 || mV === 5 || mV === 8 || mV === 10) && dV === 31) {
-            return false;
-        }
-        return true;
-    }
-    //Change the date selection, given the input. As assigned during every loop of the forEach array method.
-    [months,days,years].forEach(function(d){
-        d.addEventListener(`input`, function(){
-            //If the date doesn't exist, inform the user. Otherwise display selection.
-            if (!checkDays()) {
-                selection.innerHTML = `That date does not exist.`;
-            } else {
-                selection.innerHTML = `${1 + Number(months.value)}/${days.value}/${years.value}`;
-            }
-        });
-    });
-    //Process the number of days between the selected date and the current date.
+    //Process the number of days between the selected date and the current date when the Submit button is pressed.
     submit.addEventListener(`click`, function(){
-        const then = new Date(years.value, months.value, days.value);
+        //Create two dates; those being the current date and the date selected by the user.
+        const then = new Date(datePicker.value);
         const now = new Date();
-        if (!checkDays()) {
-            timer.innerHTML = `That date does not exist.`;
-        } else {
-            timer.innerHTML = `There are ${Math.abs(((then.getTime() - now.getTime()) / 86400000).toFixed(0))} days between today and ${1 + Number(months.value)}/${days.value}/${years.value}.`;
-        }
+        //Split the datePicker variable value into usable strings inside an array; intended to make displaying final results more appealing.
+        const datePickerArray = datePicker.value.split(`-`);
+        //Display final results.
+        timer.innerHTML = `There are ${Math.abs(((then.getTime() - now.getTime()) / 86400000).toFixed(0))} days between today and ${datePickerArray[1]}/${datePickerArray[2]}/${datePickerArray[0]}.`;
     });
 }
 main();
